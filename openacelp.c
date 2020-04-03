b69c994fda7399c338f4ec7000efb6e61ba3ada1
Wojciech Kaczmarski <44336093+sp5wwp@users.noreply.github.com>
2020-04-03 08:41:08 +0200

    Add files via upload

--- openacelp.c
+++ openacelp.c
@@ -13,9 +13,6 @@
 
 //-----------------------------ACELP includes & defines------------------------------
 //Global consts
-#include "N_POW2.h"									//powers of 2 array
-#include "LOG2.h"									//array for Log2 computation
-
 #define FRAME_LEN		20							//voice frame length in ms
 #define FRAME_SIZ		160							//voice frame samples number, 0.02s*8000Hz
 #define	WINDOW_LEN		30							//length of window for autocorrelation computation
@@ -75,16 +72,17 @@ void Window_Speech(int16_t *inp, int16_t *outp)
 }
 
 //autocorrelation r(k) computation, k=0..10
+//additional bandwidth expansion f=60Hz for f_s=8000Hz sample rate
 void Autocorr(int16_t *spch, uint32_t *r)
 {
 	uint8_t ovf;
 	uint32_t prev;
 	uint8_t norm_shift=0;		//shifts left needed to normalize r[]
 
-	memset((uint8_t*)r, 0, 11*sizeof(uint32_t));
-	
 	//initially, set r[0]=1 to avoid r[] containing only zeros
+	//zero out the rest
 	r[0]=1;
+	memset((uint8_t*)&r[1], 0, 10*sizeof(uint32_t));
 	
 	//r[0] calculation
 	//if r[0] overflows uint32_t, divide the signal by 4
@@ -101,10 +99,10 @@ void Autocorr(int16_t *spch, uint32_t *r)
 			{				
 				//divide the signal by 4
 				for(uint8_t j=0; j<WINDOW_SIZ; j++)
-					spch[j]/=4;
+					spch[j] /= 4;
 				
-				//zero r[]
-				memset((uint8_t*)r, 0, 11*sizeof(uint32_t));
+				//re-set r[0] to 1
+				r[0] = 1;
 				
 				ovf = 1;
 				
@@ -114,10 +112,6 @@ void Autocorr(int16_t *spch, uint32_t *r)
 		}
 	}
 	while(ovf);
-	
-	#ifdef DEBUG
-	printf("r[0]=%lld\n", r[0]);
-	#endif
 
 	//r[0] normalization to the uint32_t limit
 	//shift left until the MSB==1
@@ -130,23 +124,124 @@ void Autocorr(int16_t *spch, uint32_t *r)
 		}
 	}
 	
+	//r[1]..r[10] calculation
+	for(uint8_t i=1; i<=10; i++)
+	{
+		for(uint8_t j=0; j<WINDOW_SIZ; j++)
+			r[i] += spch[j] * spch[j-i];
+			
+		//normalize
+		r[i] <<= norm_shift;
+	}
+	
 	#ifdef DEBUG
 	printf("norm_shift=%d\n", norm_shift);
+	for(uint8_t i=0; i<=10; i++)
+		printf("r[%d]=%lld\n", i, r[i]);
+	printf("\n");
+	#endif
+	
+	//bandwidth expansion, f=60Hz, f_s=8000Hz
+	//r[0] is multiplied by 1.00005, which is equivalent to adding a noise floor at -43 dB
+	float w_lag[11]={1.0};	//window for bandwidth expansion, w_lag[0]=1.0
+	
+	for(uint8_t i=1; i<=10; i++)
+	{
+		w_lag[i] = exp(-0.5 * (2.0 * M_PI * 60.0 * i)/(8000.0));
+		w_lag[i] /= 1.00005;
+	}
+	
+	#ifdef DEBUG
+	for(uint8_t i=0; i<=10; i++)
+		printf("w_lag[i]=%f\n", w_lag[i]);
+	printf("\n");
+	#endif
+	
+	//window the autocorrelation values
+	for(uint8_t i=0; i<11; i++)
+		r[i] *= w_lag[i];
+	
+	#ifdef DEBUG
+	for(uint8_t i=0; i<=10; i++)
+		printf("r[%d]=%lld\n", i, r[i]);
+	printf("\n");
 	#endif
 }
 
-//LPC coefficients calculation
+//LP coefficients calculation
 //based on the Levison-Durbin algorithm
-void LD_Solver(uint32_t *r, int16_t *A)
+//arg1: modified autocorrelation matrix, arg2: LP filter coeffs
+void LD_Solver(uint32_t *r, int32_t *a)
 {
-	;
-}
-
-//innovative codebook search
-int16_t Innov_Codebook_Search(int16_t dn[], int16_t f[], int16_t h[], int16_t rr[][32],
-             int16_t cod[], int16_t y[], int16_t *sign, int16_t *shift_code)
-{
-	;
+	float v, c, e;
+	float rv[11];	//
+	float tmp[11];	//
+	//float E[11];
+	//float sum;
+	
+	/*E[0] = (float)r[0];
+	
+	for(uint8_t i=1; i<=10; i++)
+	{
+		sum=0.0;
+		
+		if(i>1)
+		{
+			sum=0.0;
+			for(uint8_t j=1; j<=i-1; j++)
+				sum += (float)a_p[j]*r[i-j];
+		}
+		
+		k = -((float)r[i]+sum)/E[i-1];
+		
+		a_p[i] = k;
+		
+		for(uint8_t j=1; j<=i-1; j++)
+		{
+			a_n[j] = a_p[j] + k * a_p[i-j];
+		}
+		
+		E[i] = (1.0-k*k) * E[i-1];
+	}*/
+	
+	rv[0]=1.0;
+	tmp[0]=1.0;
+	rv[1]=-(float)r[1]/r[0];
+	
+	v=r[0]-(float)r[1]*r[1]/r[0];
+	
+	for(uint8_t j=1; j<=10; j++)
+	{
+		e=0.0;
+		
+		for(uint8_t i=0; i<=j-1; i++)
+		{
+			e+=r[j-i]*a[i-1];
+			c=e/v;
+			v=v-c*e;
+			
+		}
+	}
+	
+	for(uint8_t i=0; i<11; i++)
+		printf("%f\n", rv[i]);
+	/*
+	#ifdef DEBUG
+	for(uint8_t i=0; i<10; i++)
+	{
+		for(uint8_t j=0; j<10; j++)
+		{
+			if(j<9)
+				printf("%lld,", r[abs(i-j)]);
+			else
+				printf("%lld;\n", r[abs(i-j)]);
+		}
+	}
+	
+	for(uint8_t i=1; i<=10; i++)
+			printf("%lld\n", r[i]);
+	#endif
+	*/
 }
 
 int main(void)
@@ -175,6 +270,8 @@ int main(void)
 		;//printf("%d\n", tst_out[i]);
 		
 	Autocorr(tst_out, r);
+	
+	LD_Solver(r, NULL);
 	
 	return 0;
 }
