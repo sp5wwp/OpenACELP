cdfeba1628f038b6a629d2d9e74f61c1b86888d7
Wojciech Kaczmarski <44336093+sp5wwp@users.noreply.github.com>
2020-04-07 16:33:04 +0200

    Add files via upload

--- scripts/q_codebook_generator.py
+++ scripts/q_codebook_generator.py
@@ -1,3 +1,9 @@
+#----------------------------------------------
+# LBG algorithm performance test
+#
+# SP5WWP 07/04/2020
+#----------------------------------------------
+
 from Q1 import q1
 from Q2 import q2
 from Q3 import q3
@@ -5,38 +11,39 @@ from Q3 import q3
 import lbg
 import time
 
-t1=time.perf_counter()
+for size in range(1, 16+1):
+    t1=time.perf_counter()
 
-#Q1 codebook
-f=open("q1.txt", "w+")
+    #Q1 codebook
+    f=open("q1.txt", "w+")
 
-cb, cb_abs_w, cb_rel_w = lbg.generate_codebook(q1, 16)
+    cb, cb_abs_w, cb_rel_w = lbg.generate_codebook(q1, size)
 
-for i, c in enumerate(cb):
-    f.write("%s\n" % (c))
-    
-f.close()
+    for i, c in enumerate(cb):
+        f.write("%s\n" % (c))
+        
+    f.close()
 
-#Q2 codebook
-f=open("q2.txt", "w+")
+    #Q2 codebook
+    f=open("q2.txt", "w+")
 
-cb, cb_abs_w, cb_rel_w = lbg.generate_codebook(q2, 16)
+    cb, cb_abs_w, cb_rel_w = lbg.generate_codebook(q2, size)
 
-for i, c in enumerate(cb):
-    f.write("%s\n" % (c))
-    
-f.close()
+    for i, c in enumerate(cb):
+        f.write("%s\n" % (c))
+        
+    f.close()
 
-#Q3 codebook
-f=open("q3.txt", "w+")
+    #Q3 codebook
+    f=open("q3.txt", "w+")
 
-cb, cb_abs_w, cb_rel_w = lbg.generate_codebook(q3, 16)
+    cb, cb_abs_w, cb_rel_w = lbg.generate_codebook(q3, size)
 
-for i, c in enumerate(cb):
-    f.write("%s\n" % (c))
-    
-f.close()
+    for i, c in enumerate(cb):
+        f.write("%s\n" % (c))
+        
+    f.close()
 
-t2=time.perf_counter()
+    t2=time.perf_counter()
 
-print("Time = %ds\n" % (int(t2-t1)))
+    print("%d\t%d\t%d\t%d" % (size, size, size, int(t2-t1)))
