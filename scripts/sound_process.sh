840c71b7a12b864654d7afc61c4ba483008a9d72
Wojciech Kaczmarski <44336093+sp5wwp@users.noreply.github.com>
2020-04-07 11:12:21 +0200

    Add files via upload

---
+++ scripts/sound_process.sh
@@ -0,0 +1,47 @@
+#!/bin/bash 
+
+#-----------------------------------------------------------
+# This script converts NIST SPHERE .sph files to .wav
+# It normalizes the amplitude to -1dB, applies a lowpass
+# at 3400Hz and resamples the output to 8000Hz
+#
+# This can is used to produce a training set
+# for LSF vector quantization using TED-LIUM speech corpus.
+#
+# SP5WWP 07/04/2020
+#-----------------------------------------------------------
+
+#convert all .sph files to .wav
+#do it if the .wav file doesn't exist already
+for f in *.sph
+do
+	if [ ! -f "${f%.*}.wav" ]
+	then
+		sox -t sph "$f" -b 16 -t wav "${f%.*}.wav"
+	fi
+done
+
+#normalize to -1dB
+for f in *.wav
+do
+	sox --norm=-1 "$f" "${f%.*}_new.wav"
+	mv "${f%.*}_new.wav" "$f"
+done
+
+#lowpass at 3.4kHz, 4 passes
+#maybe sox would be better here
+for f in *.wav
+do
+	for passes in {1..4}
+	do
+		ffmpeg -i "$f" -filter lowpass=3400 "${f%.*}_new.wav"
+		mv "${f%.*}_new.wav" "$f"
+	done
+done
+
+#resample to 8000Hz
+for f in *.wav
+do
+	sox "$f" -r 8000 "${f%.*}_new.wav"
+	mv "${f%.*}_new.wav" "$f"
+done
